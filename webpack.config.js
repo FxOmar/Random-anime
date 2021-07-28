1a1116922ee662ba6b5c9417bfce2cc497be37eb
Omar Chadidi <omar.chadidi@hi5group.co.uk>
2021-07-28 15:30:49 +0100

    Fix env

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,61 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const nodeEnv = process.env.NODE_ENV || 'development';
-const isProd = nodeEnv === 'production';
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const nodeEnv = process.env.NODE_ENV || "development";
+const isProd = nodeEnv === "production";
 
 const plugins = [
   new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify(nodeEnv)
-    }
+    "process.env": {
+      NODE_ENV: JSON.stringify(nodeEnv),
+    },
   }),
   new HtmlWebpackPlugin({
-    title: 'Typescript Webpack Starter',
-    template: '!!ejs-loader!src/index.html'
+    title: "Typescript Webpack Starter",
+    template: "!!ejs-loader!src/index.html",
   }),
   new webpack.LoaderOptionsPlugin({
     options: {
       tslint: {
         emitErrors: true,
-        failOnHint: true
-      }
-    }
-  })
+        failOnHint: true,
+      },
+    },
+  }),
 ];
 
 var config = {
-  devtool: isProd ? 'hidden-source-map' : 'source-map',
-  context: path.resolve('./src'),
+  devtool: isProd ? "hidden-source-map" : "source-map",
+  context: path.resolve("./src"),
   entry: {
-    app: './index.ts'
+    app: "./index.ts",
   },
   output: {
-    path: path.resolve('./dist'),
-    filename: '[name].bundle.js'
+    path: path.resolve("./dist"),
+    filename: "[name].bundle.js",
   },
   module: {
     rules: [
       {
-        enforce: 'pre',
+        enforce: "pre",
         test: /\.tsx?$/,
         exclude: [/\/node_modules\//],
-        use: ['awesome-typescript-loader', 'source-map-loader']
+        use: ["awesome-typescript-loader", "source-map-loader"],
       },
-      !isProd
-        ? {
-            test: /\.(js|ts)$/,
-            loader: 'istanbul-instrumenter-loader',
-            exclude: [/\/node_modules\//],
-            query: {
-              esModules: true
-            }
-          }
-        : null,
-      { test: /\.html$/, loader: 'html-loader' },
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] }
-    ].filter(Boolean)
+      { test: /\.html$/, loader: "html-loader" },
+      { test: /\.css$/, loaders: ["style-loader", "css-loader"] },
+    ].filter(Boolean),
   },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: [".ts", ".js"],
   },
   plugins: plugins,
   devServer: {
-    contentBase: path.join(__dirname, 'dist/'),
+    contentBase: path.join(__dirname, "dist/"),
     compress: true,
     port: 3000,
-    hot: true
-  }
+    hot: true,
+  },
 };
 
 module.exports = config;
