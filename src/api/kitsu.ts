bae1d4ce1c3cfec687f65468a04ff2b46bbe0b0b
Omar Chadidi <omar.chadidi@hi5group.co.uk>
2021-07-28 20:49:06 +0100

    Add the option to fetch the next page content.

--- src/api/kitsu.ts
+++ src/api/kitsu.ts
@@ -1,7 +1,10 @@
 import Http from "./http";
 import { AnimeList } from "../interfaces/animeList.interface";
 
-export async function listAll(): Promise<AnimeList> {
-  const { data } = await Http.get("anime");
+export async function listAll(page: string): Promise<AnimeList> {
+  const { data } = await Http.get(
+    `anime?page[limit]=20&page[offset]=${parseInt(page)}`
+  );
+
   return data;
 }
