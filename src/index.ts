f742102f7b9d642ab0740b7f857e51c9bea6c59b
Omar Chadidi <omar.chadidi@hi5group.co.uk>
2021-07-28 20:53:17 +0100

    Fetch anime collection
    Now I can  roll a random anime from a big list
    everytime I roll it add more

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,81 @@
-import { getAnimeList } from "./api/getAnimeList";
-import { Datum } from "./Interfaces/animeList.interface";
+import * as Kitsu from "./api/kitsu";
+import {
+  AnimeList,
+  Datum,
+  AnimeListLinks,
+} from "./Interfaces/animeList.interface";
 
+interface StateInterface {
+  collection: Datum[];
+  links: AnimeListLinks;
+  nextPage: string;
+}
 
-const app = document.getElementById("app") as HTMLDivElement;
+/**
+ * Global state
+ */
+let state: StateInterface = {
+  collection: [],
+  links: {
+    first: "",
+    next: "",
+    last: "",
+  },
+  nextPage: "0",
+};
+
+/**
+ * Fetch anime collection
+ */
+function fetchAnimeCollection() {
+  if (state.nextPage !== null) {
+    Kitsu.listAll(state.nextPage).then((response: AnimeList) => {
+      state.collection.push(...response.data);
+
+      state.nextPage = response.links.next
+        ? response.links.next.match(/[0-9]+$/g)[0]
+        : null;
+    });
+  } else {
+    return;
+  }
+}
+
+/**
+ * Get random element from an array.
+ * @param arr Array
+ */
+function randomItemFromArray<T>(arr: T[]): T {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function rollRandomAnime(arr: Datum[]) {
+  const randomAnime = randomItemFromArray(arr);
+  console.log(randomAnime);
+}
+
+const bubblyButtons = document.getElementById(
+  "bubbly-button"
+) as HTMLButtonElement;
+
+bubblyButtons.addEventListener(
+  "click",
+  function (ev) {
+    const target = ev.target as HTMLButtonElement;
+
+    ev.preventDefault;
+
+    // reset animation
+    target.classList.remove("animate");
+
+    target.classList.add("animate");
+
+    setTimeout(function () {
+      target.classList.remove("animate");
+    }, 500);
+
+    rollRandomAnime(state.collection);
+    fetchAnimeCollection();
+  },
+  false
+);
